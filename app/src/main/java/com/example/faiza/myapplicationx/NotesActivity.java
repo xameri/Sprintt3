76956fb763196de35a681783e88bf213396e7e41
xameri <32283902+xameri@users.noreply.github.com>
2018-05-09 19:06:13 -0500

    Update NotesActivity.java
    ghfghgf

--- app/src/main/java/com/example/faiza/myapplicationx/NotesActivity.java
+++ app/src/main/java/com/example/faiza/myapplicationx/NotesActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 
 public class NotesActivity extends AppCompatActivity {
 
-    Button buttonBack;
+    
     EditText editTextTitles, editTextNotes, editTextDate;
     NoteDataSource noteDataSource;
 
